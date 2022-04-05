97bc28f9654b5b3ee796c94704ed3899d526cd6a
Iulian Cozma <iulian.cozma@gmail.com>
2022-04-05 21:51:53 +0300

    sample

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask
 from waitress import serve
+from handlers import hello
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    return "Hello World! 4"
+app.add_url_rule('/', view_func=hello, methods=['GET'])
 
 
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
-    # app.run(host='0.0.0.0', port=8080)
--- run_local.py
+++ run_local.py
@@ -1,13 +1,7 @@
 from flask import Flask
 from waitress import serve
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "Hello World! 4"
-
+from handlers import hello
+from app import app
 
 if __name__ == '__main__':
     # serve(app, host='0.0.0.0', port=8080)
