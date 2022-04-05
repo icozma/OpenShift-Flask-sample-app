633d6d1b2452cac5d460ec5b82668a96f1169369
Iulian Cozma <iulian.cozma@gmail.com>
2022-04-05 22:56:54 +0300

    sample

--- run_srv.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from waitress import serve
-from handlers import hello
-from app import app
-
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
-    # app.run(host='0.0.0.0', port=8080)
